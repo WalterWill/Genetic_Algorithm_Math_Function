829bca1d07a2c73f2bac8134f947636ef60ee5f5
William Walter <will.javik@gmail.com>
2022-11-13 20:26:29 -0300

    Classe Cromossomo
    Criado a classe cromossomo, junto com o construtor que converte para binário os valores informados

---
+++ src/main/java/Cromossomo.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class Cromossomo {
+    int n_bits = 3;
+    int x[];
+    int y[];
+
+    Cromossomo(int x, int y){
+        this.x = new int[n_bits];
+        this.y = new int[n_bits];
+
+        char x_char[] = new char[n_bits];
+        char y_char[] = new char[n_bits];
+
+        String aux_x = new String();
+        aux_x = Integer.toBinaryString(x);
+
+        String aux_y = new String();
+        aux_y = Integer.toBinaryString(y);
+
+        //Fator de correção do tamanho dos vetores
+        int correction = 0;
+
+
+        //Insere o X
+        if (aux_x.length() > n_bits){
+            IllegalArgumentException erro = new IllegalArgumentException(); //Foi inserido um numero maior que o suportado
+            throw erro;
+        } else {
+            correction = this.n_bits - aux_x.length();
+            aux_x.getChars(0,aux_x.length(),x_char, (correction));
+
+            for (int i = 0; i < n_bits; i++){
+                if (x_char[i] == '1'){
+                    this.x[i] = 1;
+                } else {
+                    this.x[i] = 0;
+                }
+            }
+
+        }
+
+        //Insere o Y
+        if (aux_y.length() > n_bits){
+            IllegalArgumentException erro = new IllegalArgumentException(); //Foi inserido um numero maior que o suportado
+            throw erro;
+        } else {
+            correction = this.n_bits - aux_y.length();
+            aux_y.getChars(0,aux_y.length(),y_char, correction);
+
+            for (int i = 0; i < n_bits; i++){
+                if (y_char[i] == '1'){
+                    this.y[i] = 1;
+                } else {
+                    this.y[i] = 0;
+                }
+            }
+        }
+    }
+
+    public String toString(){
+        return (Arrays.toString(this.x).replaceAll("\\[|\\]|,|\\s", "") + Arrays.toString(this.y).replaceAll("\\[|\\]|,|\\s", ""));
+    }
+}
---
+++ src/test/java/CromossomoTest.java
@@ -0,0 +1,96 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CromossomoTest {
+    @Test
+    public void VerificaBinarioQuandoInserido1(){
+        int x = 1;
+        int y = 0;
+
+        Cromossomo a = new Cromossomo(x,y);
+
+        String valorRecebido = a.toString();
+        String valorEsperado = new String("001000");
+
+        Assert.assertEquals(valorEsperado, valorRecebido);
+    }
+
+    @Test
+    public void VerificaBinarioQuandoInserido2(){
+        int x = 2;
+        int y = 0;
+
+        Cromossomo a = new Cromossomo(x,y);
+
+        String valorRecebido = a.toString();
+        String valorEsperado = new String("010000");
+
+        Assert.assertEquals(valorEsperado, valorRecebido);
+    }
+
+    @Test
+    public void VerificaBinarioQuandoInserido3(){
+        int x = 3;
+        int y = 0;
+
+        Cromossomo a = new Cromossomo(x,y);
+
+        String valorRecebido = a.toString();
+        String valorEsperado = new String("011000");
+
+        Assert.assertEquals(valorEsperado, valorRecebido);
+    }
+
+    @Test
+    public void VerificaBinarioQuandoInserido4(){
+        int x = 4;
+        int y = 0;
+
+        Cromossomo a = new Cromossomo(x,y);
+
+        String valorRecebido = a.toString();
+        String valorEsperado = new String("100000");
+
+        Assert.assertEquals(valorEsperado, valorRecebido);
+    }
+
+    @Test
+    public void VerificaBinarioQuandoInserido5(){
+        int x = 5;
+        int y = 0;
+
+        Cromossomo a = new Cromossomo(x,y);
+
+        String valorRecebido = a.toString();
+        String valorEsperado = new String("101000");
+
+        Assert.assertEquals(valorEsperado, valorRecebido);
+    }
+
+    @Test
+    public void VerificaBinarioQuandoInserido6(){
+        int x = 6;
+        int y = 0;
+
+        Cromossomo a = new Cromossomo(x,y);
+
+        String valorRecebido = a.toString();
+        String valorEsperado = new String("110000");
+
+        Assert.assertEquals(valorEsperado, valorRecebido);
+    }
+
+    @Test
+    public void VerificaBinarioQuandoInserido7(){
+        int x = 7;
+        int y = 0;
+
+        Cromossomo a = new Cromossomo(x,y);
+
+        String valorRecebido = a.toString();
+        String valorEsperado = new String("111000");
+
+        Assert.assertEquals(valorEsperado, valorRecebido);
+    }
+}
+
